25fd2cda4b30117108f7f0e60e612b118c72ec01
2057559s <2057559s@bo1028-3-12u.dcs.gla.ac.uk>
2016-12-05 11:43:53 +0000

    Compiling

--- comicScraper.hs
+++ comicScraper.hs
@@ -1,13 +1,53 @@
 {-# LANGUAGE OverloadedStrings #-}
 import Data.List (isInfixOf)
+import Data.Sequence
+import Data.Char
 import Text.HTML.Scalpel
 import Control.Monad
 import Control.Applicative
 main :: IO()
 main = do
-    res <- scrapeURL "http://www.gla.ac.uk/schools/computing/staff/" scrapeStaffURL
-    print res
+    links <- getLinks "http://www.gla.ac.uk//schools/computing/staff"
+    details <- return (fmap (mapM getDetails) links)
+    sequence_ details
+    print links
 
+
+
+--Function for scraping the main link
+getLinks :: String -> IO (Maybe [String])
+getLinks a = scrapeURL a scrapeLinks
+
+-- Function for scraping each individual staff link
+getDetails :: String -> IO (Maybe [String])
+getDetails a = scrapeURL a scrapeDetails
+
+
+scrapeDetails :: Scraper String [String]
+scrapeDetails =  
+    chroots "h1" $ do
+        name <- text anySelector
+        let n = "name: " ++ name
+        return n
+
+
+scrapeLinks :: Scraper String [String]
+scrapeLinks =  
+    chroots "a" $ do
+        altText <- attr "href" anySelector
+        guard ("schools/computing/staff" `isInfixOf` altText)
+        let url = "http://www.gla.ac.uk/" ++ altText
+        return url
+
+
+
+
+
+
+
+
+
+{-
 scrapeStaffURL :: Scraper String [[String]]
 scrapeStaffURL =
     chroots ("a") scrapeTitle
@@ -18,8 +58,9 @@ scrapeTitle = do
     staffLink <- attr "href" anySelector
     guard ("schools/computing/staff/" `isInfixOf` staffLink)
     let url = "www.gla.ac.uk" ++ staffLink
+    print url
 
-    
+    -}
 
 
 
